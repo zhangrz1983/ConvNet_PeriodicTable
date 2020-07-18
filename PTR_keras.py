00e7aa60738b24aa2ce2961cfe77817fe7832d62
Rui-Zhi Zhang <nwuzhangrz@gmail.com>
2020-07-18 11:44:28 +0100

    Update PTR_keras.py

--- PTR_keras.py
+++ PTR_keras.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
-with open ('heusler') as fin:
+with open ('Data/Heusler_energy') as fin:
     lines = fin.readlines()
 ndata=len(lines)
 
